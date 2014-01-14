13830106bc57df92a8bf25821213f0cc213e716f
Richard <ripark87@gmail.com>
2014-01-14 13:00:42 -0500

    Hero Movement/Animation

---
+++ Assets/Scripts/HeroMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMovement : MonoBehaviour 
+{	
+		
+		public Vector2 speed = new Vector2(50, 50);
+		private Vector2 movement;
+		Animator anim;
+
+		void Start()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+
+		void Update()
+	{
+			float inputX = Input.GetAxis("Horizontal");
+			float inputY = Input.GetAxis("Vertical");
+
+			movement = new Vector2(
+				speed.x * inputX,
+				speed.y * inputY);
+			
+			anim.SetFloat("Speed", Mathf.Abs(inputX));
+
+
+		//Mouse control 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			transform.LookAt(ray.GetPoint(-1000),Vector3.forward);
+
+	}
+		
+		void FixedUpdate()
+	{
+			rigidbody2D.velocity = movement;
+	}
+}
