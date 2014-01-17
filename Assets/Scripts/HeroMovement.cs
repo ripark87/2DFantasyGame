00899bec30f1f6b4d6cb768515bae735e3c07493
Brian Kang <btk226@nyu.edu>
2014-01-17 03:05:01 -0500

    Roll mechanic; folder organization; moved around character in test screen

--- Assets/Scripts/HeroMovement.cs
+++ Assets/Scripts/HeroMovement.cs
@@ -2,45 +2,54 @@
 using System.Collections;
 
 public class HeroMovement : MonoBehaviour 
-{	
-		
-		public Vector2 speed = new Vector2(50, 50);
-		private Vector2 movement;
-		Animator anim;
+{
+    //how powerful we want the dodge force to be, set in inspector
+    public int power = 10;
+    Ray ray;
 
-		void Start()
+	public Vector2 speed = new Vector2(50, 50);
+	private Vector2 movement;
+    Animator anim;
+
+	void Start()
 	{
-		anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
 	}
 
 
-		void Update()
+	void Update()
 	{
-			float inputX = Input.GetAxis("Horizontal");
-			float inputY = Input.GetAxis("Vertical");
+        //hero looks toward mouse cursor
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        transform.LookAt(ray.GetPoint(-1000), Vector3.forward);
 
-            //enable animation for both horizontal and vertical.
-            float temp = Mathf.Abs(inputX + inputY);
-
-			movement = new Vector2(
+        //movement controls
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+ 
+		movement = new Vector2(
 				speed.x * inputX,
 				speed.y * inputY);
 			
-
-			anim.SetFloat("Speed", Mathf.Abs(inputX + inputY));
-
-			anim.SetFloat("Speed", temp);
-
-
-
-		//Mouse control 
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-			transform.LookAt(ray.GetPoint(-1000),Vector3.forward);
-
+        //play movement animation as long as player is moving
+        anim.SetFloat("Speed", Mathf.Abs(inputX + inputY));
+        
 	}
 		
-		void FixedUpdate()
+	void FixedUpdate()
 	{
-			rigidbody2D.velocity = movement;
+		rigidbody2D.velocity = movement;
+
+        //calculate the direction of roll, turning mouse position into world coordinates
+        Vector2 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (worldMousePos - new Vector2(transform.position.x, transform.position.y)).normalized;
+        //Vector2 mouseLook = (Input.mousePosition - transform.position).normalized;
+
+        //rolling or dashing controls; when space is pressed, add force in direction hero is facing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidbody2D.AddForce(direction * power);
+            
+        }
 	}
 }
